a30410d8637988f403e78edf35029bc745ce3866
lipengfei <lipengfei@shopee.com>
2022-03-24 10:35:34 +0800

    add BizContext

--- app/accountcenter-common/src/main/java/pers/hubery/accountcenter/common/context/BizContext.java
+++ app/accountcenter-common/src/main/java/pers/hubery/accountcenter/common/context/BizContext.java
@@ -1,11 +1,14 @@
 package pers.hubery.accountcenter.common.context;
 
+import lombok.Data;
+
 /**
  * 业务上下文
  *
  * @author hubery
  * @version BizContext.java v1.0 2022-03-24 10:18
  */
+@Data
 public class BizContext {
 
     /** 租户ID */
@@ -13,6 +16,5 @@ public class BizContext {
 
     /** 账本 */
     private String accountBook;
-
 
 }
--- app/accountcenter-common/src/main/java/pers/hubery/accountcenter/common/context/BizContextHolder.java
+++ app/accountcenter-common/src/main/java/pers/hubery/accountcenter/common/context/BizContextHolder.java
@@ -10,4 +10,61 @@ public class BizContextHolder {
 
     /** 业务上下文线程变量 */
     private static final ThreadLocal<BizContext> BIZ_CONTEXT_THREAD_LOCAL = new ThreadLocal<BizContext>();
+
+    /**
+     * 获取业务上下文
+     *
+     * @return 业务上下文
+     */
+    public static BizContext get() {
+        return BIZ_CONTEXT_THREAD_LOCAL.get();
+    }
+
+    /**
+     * 设置业务上下文
+     *
+     * @param bizContext 业务上下文
+     */
+    public static void set(BizContext bizContext) {
+        BIZ_CONTEXT_THREAD_LOCAL.set(bizContext);
+    }
+
+    /**
+     * 清理业务上下文
+     */
+    public static void clear() {
+        BIZ_CONTEXT_THREAD_LOCAL.remove();
+    }
+
+    /**
+     * 从上下文中获取 accountBook
+     *
+     * @return accountBook
+     */
+    public static String getAccountBook() {
+
+        BizContext bizContext = get();
+
+        if (bizContext == null) {
+            return null;
+        }
+
+        return bizContext.getAccountBook();
+    }
+
+    /**
+     * 从上下文中获取 tenantId
+     *
+     * @return tenantId
+     */
+    public static String getTenantId() {
+
+        BizContext bizContext = get();
+
+        if (bizContext == null) {
+            return null;
+        }
+
+        return bizContext.getTenantId();
+    }
 }
